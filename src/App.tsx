2d254a92645da6255a83bdc8e4426096e5eeb8f1
Luis Henrique Jacinto <luishjacinto98@gmail.com>
2023-09-11 22:46:42 -0300

    chore: configure shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Button>Hello</Button>
     </>
   )
 }
 
-export App
+export { App }
